681352849a8d1f725ff04ac0b1dd4904c09edc7e
hexdump0815 <hexdump0815@googlemail.com>
2020-12-18 19:17:05 +0100

    v5.10.1, mainline, defconfig based + rtl8723cs added

--- misc.rkc/options/enable-additional-options.sh
+++ misc.rkc/options/enable-additional-options.sh
@@ -20,6 +20,9 @@ cd /compile/source/linux-stable-rk
 ./scripts/config -d CONFIG_REISERFS_FS
 ./scripts/config -d CONFIG_OCFS2_FS
 
+./scripts/config -d CONFIG_LOCALVERSION_AUTO
+./scripts/config --set-str CONFIG_LOCALVERSION "-stb-rkc"
+
 for i in `cat /compile/doc/stable-rk/misc.rkc/options/additional-options-*-yes.txt`; do
   echo $i
   ./scripts/config -e $i
